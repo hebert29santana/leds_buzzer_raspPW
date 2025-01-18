525fd5f1655921dcf6d86705328f4d91fa2f825d
Habib <habibmariavalentina@gmail.com>
2025-01-18 15:04:10 -0300

    Controle dos leds

---
+++ controle_leds.h
@@ -0,0 +1,18 @@
+#ifndef CONTROLE_LEDS_H
+#define CONTROLE_LEDS_H
+
+// Definições dos pinos dos LEDs
+#define LED_VERDE_PIN 11
+#define LED_AZUL_PIN 12
+#define LED_VERMELHO_PIN 13
+
+// Definições das teclas
+#define TECLA_A 'A'
+#define TECLA_B 'B'
+#define TECLA_C 'C'
+#define TECLA_D 'D'
+
+// Declaração da função controle_leds
+void controle_leds(char tecla);
+
+#endif // CONTROLE_LEDS_H
