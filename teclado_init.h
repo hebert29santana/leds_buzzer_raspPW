d517da481d61780bfc04328324f5d41893e5c1c1
pc500ftgi <145073580+pc500ftgi@users.noreply.github.com>
2025-01-18 19:51:16 -0300

    Teclado

---
+++ teclado_init.h
@@ -0,0 +1,17 @@
+#ifndef TECLADO_INIT_H
+#define TECLADO_INIT_H
+
+// Definições dos pinos das linhas e colunas do teclado matricial
+#define ROW1 2  // Exemplo: substitua pelos números reais dos pinos
+#define ROW2 3
+#define ROW3 4
+#define ROW4 5
+#define COL1 6
+#define COL2 7
+#define COL3 8
+#define COL4 9
+
+// Declaração da função de inicialização do teclado matricial
+void teclado_init();
+
+#endif // TECLADO_H
